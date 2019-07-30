c11e3287ef2ed540d932c675234330a09867ff67
姜灏 <shymuel1998@163.com>
2019-07-30 14:02:56 +0800

    Update mission3.py

--- human_code/missions/mission3.py
+++ human_code/missions/mission3.py
@@ -5,10 +5,6 @@ import math
 import numpy as np
 from matplotlib import pyplot as plt
 
-def swap(t1, t2):
-    t2, t1 = t1, t2
-    return
-
 cap = cv.VideoCapture('03.mp4')
 
 while True:
@@ -16,21 +12,24 @@ while True:
     x, y = img.shape[0:2]
     img=cv.resize(img, (int(y/1), int(x/1)))
 
+    kernel = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))  # 定义矩形结构元素
+    img = cv.morphologyEx(img, cv.MORPH_OPEN, kernel, iterations=1) #开运算
+    img = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel, iterations=1)  # 闭运算1
+
     gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
     # xgrad = cv.Sobel(gray, cv.CV_16SC1, 1, 0) #x方向梯度
     # ygrad = cv.Sobel(gray, cv.CV_16SC1, 0, 1) #y方向梯度
     # edge_output = cv.Canny(xgrad, ygrad, 50, 150)
-    edge_output = cv.Canny(gray, 80, 255)
+    edge_output = cv.Canny(gray, 60, 180)
 
     cnts, hierarchy = cv.findContours(edge_output,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
 
-    for i in cnts.size():
-        area = cv.contourArea(cnts[i])
+    for c in cnts:
+        area = cv.contourArea(c)
         # print(area)
-        swap(cnts[i], cnts[cnts.size() - 1]);
-        cnts.pop_back();
+        if ((area > 500) & (area < 4000)):
+            cv.drawContours(img, c, -1, (0, 0, 255), 3)
 
-    cv.drawContours(img,cnts,-1,(0,0,255),3)
     # print(cnts)
     cv.imshow('circle', img)
     cv.imshow('Canny', edge_output)
